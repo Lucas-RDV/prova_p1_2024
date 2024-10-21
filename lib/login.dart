2d121ceaa90706fe3156b72732db6b0028ecb1ef
KillkillerBR <lucas.vesco@gmail.com>
2024-10-21 16:34:02 -0400

    entrega sharedPreferences

--- lib/login.dart
+++ lib/login.dart
@@ -2,6 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:novo_projeto/autenticacao/sharedSessao.dart';
 
 class Login extends StatelessWidget {
+  const Login({super.key});
+
+  bool _isLoginValid(String login) {
+    if (login.isEmpty) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  bool _isSenhaValid(String senha) {
+    if (senha.isEmpty) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -26,12 +44,37 @@ class Login extends StatelessWidget {
           ),
           Row(mainAxisAlignment: MainAxisAlignment.center, children: [
             ElevatedButton.icon(
-              onPressed: () {},
+              onPressed: () {
+                if (_isLoginValid(usuarioController.text) &&
+                    _isSenhaValid(senhaController.text)) {
+                  Navigator.pushNamed(context, '/listagem');
+                      senhaController.text = "";
+                      usuarioController.text = "";
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                        content: Text('Usuario ou senha incorretos!')),
+                  );
+                }
+              },
               icon: Icon(Icons.login),
               label: Text('Entrar'),
             ),
             ElevatedButton.icon(
-              onPressed: () {},
+              onPressed: () async {
+                if (_isLoginValid(usuarioController.text) &&
+                    _isSenhaValid(senhaController.text)) {
+                      await SharedSessao.salvarToken(usuarioController.text);
+                      Navigator.pushNamed(context, '/listagem');
+                      senhaController.text = "";
+                      usuarioController.text = "";
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                        content: Text('Usuario ou senha incorretos!')),
+                  );
+                }
+              },
               icon: Icon(Icons.add_circle_outline_sharp),
               label: Text('Cadastar'),
             )
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:novo_projeto/cadastro.dart';
 import 'package:novo_projeto/controle/pessoaController.dart';
 import 'package:novo_projeto/listagem.dart';
 import 'package:novo_projeto/login.dart';
+import 'package:novo_projeto/autenticacao/sharedSessao.dart';
 
 void main(List<String> args) {
   runApp(const App());
@@ -19,16 +20,35 @@ class App extends StatefulWidget {
 
 class _App extends State<App> {
   PessoaController pessoaController = PessoaController();
+  final Future<String?> token = SharedSessao.carregarToken();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // home: const Listagem(),
       initialRoute: '/',
+
       routes: {
-        '/login': (context) => Login(),
+        '/': (context) => _carregaHome(),
         '/listagem': (context) => Listagem(pessoaController: pessoaController),
         '/cadastro': (context) => Cadastro(pessoaController: pessoaController),
+      },
+    );
+  }
+
+  Widget _carregaHome() {
+    return FutureBuilder<String?>(
+      future: token,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator(); // ou uma tela de loading
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Erro ao carregar token'));
+        } else {
+          return snapshot.data == null
+              ? Login()
+              : Listagem(pessoaController: pessoaController);
+        }
       },
     );
   }
